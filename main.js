5b00783ed25502c74f0e734b7f7dbc104f02f6de
Daan <ritsu@cumallover.me>
2018-05-06 15:56:12 +0200

    Optimizations

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 const fs = require("fs");
 const sleep = require("sleep");
-const os = require("os");
-const spawn = require("child_process");
+const spawn = require("child_process").exec;
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-var prompt = require("prompt-sync-history")
-const readline = require('readline');
 var board = process.argv[2];
 var threadID = process.argv[3];
-// TODO: refactor, and remove spaghest
+// TODO: refactor, and remove spaghet
 function cleanup() {
     fs.unlink(threadID);
-   // fs.unlink(threadID + ".1")
+    dom.window.close();
     return 0;
 }
-
 function getPage() {
     var url = "http://boards.4chan.org/" + board + "/thread/" + threadID;
-    spawn.exec("wget " + url); // im sure there's a better way to do this. maybe with JSDOM?
+    spawn("wget " + url); // im sure there's a better way to do this. maybe with JSDOM?
+    return 0;
 }
 
 getPage(); 
@@ -27,6 +24,6 @@ const dom = new JSDOM(file);
 var images = dom.window.document.getElementsByClassName("fileThumb");
 for(i = images.length-1, z=1;0 < i;i--,z++) {
     console.log('[%d / %d] Downloading: %s', z, images.length, images.item(i).toString().split("//")[1])
-    spawn.exec("cd images;wget " + images.item(i).toString().split("//")[1], (err, stdout) => null);
+    spawn("cd images;wget " + images.item(i).toString().split("//")[1], (err, stdout) => null);
 }
 cleanup();
