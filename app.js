42369b557b0982f4d7a605ed39c3b4dafc86e99c
2dook <302088476@rocmondriaan.nl>
2019-01-28 10:33:55 +0100

    Rewrite program

--- app.js
+++ app.js
@@ -1,39 +1,19 @@
 const fs = require("fs");
-const spawn = require("child_process").exec;
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const board = process.argv[2];
 const threadID = process.argv[3];
-const m = require("./func.js");
-let flag = false;
 
 // TODO: Make a graphical user interface with Electron.
 
-if (!board || !threadID) {
-    console.log("No board specified. Exiting.");
-    console.log("Correct usage: ./node app.js board threadid")
-    process.exit(1);
+async () => {
+    await fetch("www.google.com");
 }
 
-if (fs.existsSync("./" + threadID)) {
-    fs.unlink();
-    flag = true; // Sets flag to true. getPage method will check if true, if true it will skip downloading the page. Making the application faster.
-    console.log("File already exists. Skipping.")
-}
+// for (i = images.length - 1, z = 1; 0 < i; i-- , z++) {
 
-m.getPage(flag);
-
-let file = fs.readFileSync("./" + threadID, "utf8");
-const dom = new JSDOM(file);
-let images = dom.window.document.getElementsByClassName("fileThumb");
-
-for (i = images.length - 1, z = 1; 0 < i; i-- , z++) {
-
-    console.log('[%d / %d] Downloading: %s', z, images.length-1, images.item(i).toString().split("//")[1])
-    spawn("cd images;wget " + images.item(i).toString().split("//")[1], (err, stdout, stderr) => (err, stdout, stderr) {
-        console.log(err);
-    });
-}
-
-fs.unlinkSync("./" + threadID);
-dom.window.close();
+//     console.log('[%d / %d] Downloading: %s', z, images.length-1, images.item(i).toString().split("//")[1])
+//     spawn("cd images;wget " + images.item(i).toString().split("//")[1], (err, stdout, stderr) => (err, stdout, stderr) {
+//         console.log(err);
+//     });
+// }
