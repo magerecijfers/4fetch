eeb412520f173795bd6d6da46482b9774be3fd03
2dook <302088476@rocmondriaan.nl>
2019-01-29 23:22:42 +0100

    Initial commit

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 4FETCH is an application that downloads images in 4chan threads. Nothing too complex.
 
+
+[INSTRUCTIONS]
--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const fs = require("fs");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const axios = require('axios');
 const board = process.argv[2];
 const threadID = process.argv[3];
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 // TODO: Make a graphical user interface with Electron.
+async function getPageData(board, threadID) {
 
-async () => {
-    await fetch("www.google.com");
+const m = await axios.get(`https://104.16.116.221/${board}/thread/${threadID}`);
+console.log(m);
+// const data = await m.text();
+// // console.log(data);
 }
-
+getPageData("wg",7352435);
+// getPageData("wg", 4343);
 // for (i = images.length - 1, z = 1; 0 < i; i-- , z++) {
 
 //     console.log('[%d / %d] Downloading: %s', z, images.length-1, images.item(i).toString().split("//")[1])
